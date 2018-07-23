5c216fa7b910afd0f88217701ebce4ec3a64acb0
Giancarlo Sanz <gsanz95@gmail.com>
2018-07-22 22:29:02 -0500

    viewproduct.html designed not very functional but looks good.

--- js/functionality.js
+++ js/functionality.js
@@ -1,21 +1,15 @@
-function removeItem() {
-    var elem = document.getElementById('item1');
-    elem.parentNode.removeChild(elem);
+function removeItem(elem) {
+    var e = elem.parentElement.parentElement;
+    e.parentNode.removeChild(e);
     return false;
 }
-function pageInit() {
-    // Hook up the button
-    var btn = document.getElementById('btnRemove1');
-    if (btn.addEventListener) {
-        // DOM2 standard
-        btn.addEventListener('click', removeItem, false);
-    }
-    else {
-        // Really old or non-standard browser, try DOM0
-        btn.onclick = removeItem;
-    }
-}
-function checkout() {
-    window.location.href='./checkout.html';
 
+function checkout() {
+    //TO-DO Check out and store in database
+    window.location.href='./checkout.html';
+}
+
+function addToCart(){
+    //TO-DO: Add to cart
+    window.location.href='./cart.html';
 }
