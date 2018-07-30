5576cd97476dbc636a13a273b1b58a690843e829
Giancarlo Sanz <gsanz95@gmail.com>
2018-07-29 20:03:34 -0500

    Hid pagination in order to hide non-functional parts

--- home.php
+++ home.php
@@ -4,42 +4,70 @@ include "sidebar.php";
 include "cartbutton.php";
 ?>
 
-        <!-- Page Content -->
+<!-- Page Content -->
 <div class='col-lg-10 h-100 col-sm-9 col-12'>
     <div id='page-header' class='alert-heading'>
         <h1 class='text-center font-weight-bold' aria-label='Welcome to the PokeShop!'>Welcome to the PokeShop!</h1>
     </div>
     <div class='h-auto'>
-        <!-- Featured -->
+
+<?php
+
+include "config.php";
+include "functions.php";
+
+// Featured Items (Currently Random)
+$featured_items = array();
+for ($i = 0; $i < 5; $i++){
+    $id = rand(1,50);
+    if(!in_array($id,$featured_items)) {
+        array_push($featured_items, $id);
+    }else{
+        $i--;
+    }
+}
+
+//Most Popular (Currently IDs 1-5)
+$popular_items = array(1,2,3,4,5);
+
+//Current Deals (Currently 46-50)
+$deal_items = array(46,47,48,49,50);
+
+// Start Section
+echo "<!-- Featured -->
         <div class='h-auto'>
-            <h2>Featured Items</h2>
-            <div class='card-deck p-4'>
-                <div class='card bg-light text-center'>
-                    <img src='images/ArticunoThumb.png' class='card-img-top' alt='Product Img.'>
-                    <a href='viewproduct.php' name='Articuno'>Articuno ($20.00)</a>
-                </div>
+            <h2>Featured Items</h2>";
 
-                <div class='card bg-light text-center'>
-                    <img src='images/ZapdosThumb.png' class='card-img-top' alt='Product Img.'>
-    <a href='viewproduct.php' name='Zapdos'>Zapdos ($20.00)</a>
-                </div>
+// Start Deck
+echo "<div class='card-deck p-4'>";
 
-                <div class='card bg-light text-center'>
-                    <img src='images/PidgeottoThumb.png' class='card-img-top' alt='Product Img.'>
-    <a href='viewproduct.php' name='Pidgeotto'>Pidgeotto ($20.00)</a>
-                </div>
+// Print Items
+foreach ($featured_items as $item){
+    $featured_query = "SELECT * FROM products WHERE p_id = '" . $item ."'";
+    $featured_obj = mysqli_query($conn,$featured_query);
+    $featured_row = mysqli_fetch_row($featured_obj);
 
-                <div class='card bg-light text-center'>
-                    <img src='images/SnorlaxThumb.png' class='card-img-top' alt='Product Img.'>
-    <a href='viewproduct.php' name='Snorlax'>Snorlax ($20.00)</a>
-                </div>
+    // Text Manipulation
+    $featured_row[1] = ucwords($featured_row[1]);
+    $featured_row[2] = tokenTruncate($featured_row[2],50);
 
-                <div class='card bg-light text-center'>
-                    <img src='images/CharizardThumb.png' class='card-img-top' alt='Product Img.'>
-    <a href='viewproduct.php' name='Charizard'>Charizard ($20.00)</a>
-                </div>
+    echo "<div class='card bg-light text-center'>
+            <img src='./images/$featured_row[1]thumb.png' class='card-img-top img-fluid' alt='Product Img.' width='400'>
+            <div class='card-body'>
+                <a href='viewproduct.php?p_id=$featured_row[0]'><h5 class='text-center card-title'>$featured_row[1]</h5></a>
+                <p>$featured_row[2]</p>
             </div>
-            <nav aria-label='Page Navigation'>
+            <div class='card-footer text-center'>
+                    <a type='button' class='btn btn-info' aria-label='View product' href='viewproduct.php?p_id=$featured_row[0]'>View</a>
+            </div>
+        </div>";
+}
+
+// End Deck
+echo "</div>";
+
+// TO-DO Make pagination functional
+/*echo "<nav aria-label='Page Navigation'>
                 <ul class='pagination justify-content-center'>
                     <li class='page-item active'>
                         <a class='page-link' href='#'>1 <span class='sr-only'>(current)</span></a></li>
@@ -47,36 +75,46 @@ include "cartbutton.php";
                     </li>
                     <li class='page-item'><a class='page-link' href='#'>3</a></li>
                 </ul>
-            </nav>
+            </nav>";*/
 
-        </div>
-        <!-- Popular -->
+// End Section
+echo "</div>";
+
+// Start Section
+echo "<!-- Popular -->
         <div class='h-auto'>
-            <h2>Most Popular</h2>
-            <div class='card-deck p-4'>
-                <div class='card bg-light text-center'>
-                    <img src='images/CharmanderThumb.png' class='card-img-top' alt='Product Img.'>
-    <a href='viewproduct.php' name='Charmander'>Charmander ($20.00)</a>
-                </div>
-                <div class='card bg-light text-center'>
-                    <img src='images/BlastoiseThumb.png' class='card-img-top' alt='Product Img.'>
-    <a href='viewproduct.php' name='Blastoise'>Blastoise ($20.00)</a>
-                </div>
-                <div class='card bg-light text-center'>
-                    <img src='images/LaprasThumb.png' class='card-img-top' alt='Product Img.'>
-    <a href='viewproduct.php' name='Lapras'>Lapras ($20.00)</a>
-                </div>
-                <div class='card bg-light text-center'>
-                    <img src='images/GyaradosThumb.png' class='card-img-top' alt='Product Img.'>
-    <a href='viewproduct.php' name='Gyarados'>Gyarados ($20.00)</a>
-                </div>
-                <div class='card bg-light text-center'>
-                    <img src='images/DratiniThumb.png' class='card-img-top' alt='Product Img.'>
-    <a href='viewproduct.php' name='Dratini'>Dratini ($20.00)</a>
-                </div>
-            </div>
+            <h2>Most Popular</h2>";
 
-            <nav aria-label='Page Navigation'>
+// Start Deck
+echo "<div class='card-deck p-4'>";
+
+// Print Items
+foreach ($popular_items as $item){
+    $featured_query = "SELECT * FROM products WHERE p_id = '" . $item ."'";
+    $featured_obj = mysqli_query($conn,$featured_query);
+    $featured_row = mysqli_fetch_row($featured_obj);
+
+    // Text Manipulation
+    $featured_row[1] = ucwords($featured_row[1]);
+    $featured_row[2] = tokenTruncate($featured_row[2],50);
+
+    echo "<div class='card bg-light text-center'>
+            <img src='./images/$featured_row[1]thumb.png' class='card-img-top img-fluid' alt='Product Img.' width='400'>
+            <div class='card-body'>
+                <a href='viewproduct.php?p_id=$featured_row[0]'><h5 class='text-center card-title'>$featured_row[1]</h5></a>
+                <p>$featured_row[2]</p>
+            </div>
+            <div class='card-footer text-center'>
+                    <a type='button' class='btn btn-info' aria-label='View product' href='viewproduct.php?p_id=$featured_row[0]'>View</a>
+            </div>
+        </div>";
+}
+
+// End Deck
+echo "</div>";
+
+// TO-DO Make pagination functional
+/*echo "<nav aria-label='Page Navigation'>
                 <ul class='pagination justify-content-center'>
                     <li class='page-item active'>
                         <a class='page-link' href='#'>1 <span class='sr-only'>(current)</span></a></li>
@@ -84,37 +122,46 @@ include "cartbutton.php";
                     </li>
                     <li class='page-item'><a class='page-link' href='#'>3</a></li>
                 </ul>
-            </nav>
-        <!-- Deals -->
+            </nav>";*/
+
+// End Section
+echo "</div>";
+
+// Start Section
+echo   "<!-- Deals -->
         <div class='h-auto'>
-            <h2>Current Deals</h2>
-            <div class='card-deck p-4'>
-                <div class='card bg-light text-center'>
-                    <img src='media/earth.jpg' class='card-img-top' alt='Product Img.'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, quae.</p>
-                </div>
+            <h2>Current Deals</h2>";
 
-                <div class='card bg-light text-center'>
-                    <img src='media/earth.jpg' class='card-img-top' alt='Product Img.'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, quae.</p>
-                </div>
+// Start Deck
+echo "<div class='card-deck p-4'>";
 
-                <div class='card bg-light text-center'>
-                    <img src='media/earth.jpg' class='card-img-top' alt='Product Img.'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, quae.</p>
-                </div>
+// Print Items
+foreach ($deal_items as $item){
+    $featured_query = "SELECT * FROM products WHERE p_id = '" . $item ."'";
+    $featured_obj = mysqli_query($conn,$featured_query);
+    $featured_row = mysqli_fetch_row($featured_obj);
 
-                <div class='card bg-light text-center'>
-                    <img src='media/earth.jpg' class='card-img-top' alt='Product Img.'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, quae.</p>
-                </div>
+    // Text Manipulation
+    $featured_row[1] = ucwords($featured_row[1]);
+    $featured_row[2] = tokenTruncate($featured_row[2],50);
 
-                <div class='card bg-light text-center'>
-                    <img src='media/earth.jpg' class='card-img-top' alt='Product Img.'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, quae.</p>
-                </div>
+    echo "<div class='card bg-light text-center'>
+            <img src='./images/$featured_row[1]thumb.png' class='card-img-top img-fluid' alt='Product Img.' width='400'>
+            <div class='card-body'>
+                <a href='viewproduct.php?p_id=$featured_row[0]'><h5 class='text-center card-title'>$featured_row[1]</h5></a>
+                <p>$featured_row[2]</p>
             </div>
-            <nav aria-label='Page Navigation'>
+            <div class='card-footer text-center'>
+                    <a type='button' class='btn btn-info' aria-label='View product' href='viewproduct.php?p_id=$featured_row[0]'>View</a>
+            </div>
+        </div>";
+}
+
+// End Deck
+echo "</div>";
+
+// TO-DO Make pagination functional
+/*echo "<nav aria-label='Page Navigation'>
                 <ul class='pagination justify-content-center'>
                     <li class='page-item active'>
                         <a class='page-link' href='#'>1 <span class='sr-only'>(current)</span></a></li>
@@ -122,8 +169,12 @@ include "cartbutton.php";
                     </li>
                     <li class='page-item'><a class='page-link' href='#'>3</a></li>
                 </ul>
-            </nav>
-        </div>
+            </nav>";*/
+
+// End Section
+echo "</div>";
+?>
+
     </div>
 </div>
 
